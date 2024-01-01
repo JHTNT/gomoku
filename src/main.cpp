1ccdeec0df185af897393a1ad1dfb36c7b10520e
JHTNT <jerry1106666@gmail.com>
2024-01-01 17:21:18 +0800

    chore: imporve UI

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
+#include <windows.h>
+
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
-#include <windows.h>
 
 #include "core/minimax.h"
 
@@ -10,34 +11,36 @@
 using namespace std;
 
 void aiPlay(Board& board, Color color, int depth) {
-    clock_t s, e;
-    s = clock();
     auto [score, move, path] =
         minimax(board, board.ai_color, depth + 8, 0, -MAX, MAX, Points{}, true, false);
-    if (score >= 100000) {
+
+    if (score >= 10000000) {
         board.putStone(move, board.ai_color);
-        e = clock();
         board.printBoard();
-        cout << board.cnt << "\n";
-        cout << (double) (e - s) / CLOCKS_PER_SEC << " s\n";
-        cout << "Score: " << score << ", Move: [" << move.first << ", " << move.second << "]\n";
+        cout << "Score: " << score << ", Move: [" << (15 - move.first) << ", "
+             << (char) (move.second + 'A') << "]\n";
         return;
     }
+
     tie(score, move, path) =
         minimax(board, board.ai_color, depth, 0, -MAX, MAX, Points{}, false, false);
     board.putStone(move, board.ai_color);
-    e = clock();
     board.printBoard();
-    cout << board.cnt << "\n";
-    cout << (double) (e - s) / CLOCKS_PER_SEC << " s\n";
-    cout << "Score: " << score << ", Move: [" << move.first << ", " << move.second << "]\n";
+    cout << "Score: " << score << ", Move: [" << (15 - move.first) << ", "
+         << (char) (move.second + 'A') << "]\n";
 }
 
 int main() {
     SetConsoleOutputCP(CP_UTF8);
     int size = 15;
+    int depth;
     char color_input;
     Color player_color;
+    clock_t s, e;
+
+    cout << "Enter search depth: ";
+    cin >> depth;
+
     while (true) {
         cout << "Choose White or Black[b/w]: ";
         cin >> color_input;
@@ -49,11 +52,17 @@ int main() {
             break;
         }
     }
+
     Board board(size, ~player_color);
+    board.printBoard();
 
     int row, col, row_input;
     string col_input;
-    if (player_color == Color::WHITE) aiPlay(board, board.ai_color, 4);
+    bool ai_first_stone = false;
+    if (player_color == Color::WHITE)
+        aiPlay(board, board.ai_color, 4);
+    else
+        ai_first_stone = true;
     while (true) {
         cout << "Enter move[row (1 ~ 15) column (A ~ O)]: ";
         cin >> row_input >> col_input;
@@ -67,6 +76,15 @@ int main() {
             continue;
         }
         board.cnt = 0;
-        aiPlay(board, board.ai_color, 4);
+        s = clock();
+        if (ai_first_stone) {
+            aiPlay(board, board.ai_color, 4);
+            ai_first_stone = false;
+        } else {
+            aiPlay(board, board.ai_color, depth);
+        }
+        e = clock();
+        cout << board.cnt << "\n";
+        cout << (double) (e - s) / CLOCKS_PER_SEC << " s\n";
     }
 }
